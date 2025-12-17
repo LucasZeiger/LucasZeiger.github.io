ecec689db1378f927fd7c17b29507bbc83da5649
LucasZeiger <lucas.b.zeiger@gmail.com>
2025-12-17 14:29:10 +0000

    Update about.tsx

--- data/about.tsx
+++ data/about.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 export const AVAILABILITY = {
   status: true, // Set to false to turn the dot grey
-  label: "Let's collaborate!"
+  label: (
+    <a
+      href="https://linkedin.com/in/lucas-zeiger/"
+      target="_blank"
+      rel="noreferrer"
+      className="hover:text-emerald-400 transition-colors"
+    >
+      Reach out!
+    </a>
+  )
 };
 
 export const ABOUT_TEXT = (
@@ -12,12 +21,12 @@ export const ABOUT_TEXT = (
     <p>Based at the CRUK Scotland Institute in Glasgow, Scotland, I work with an excellent multidisciplinary team in the computational biology group, led by Prof. Crispin Miller.</p>
     <p>
       Please reach out if any of my research interests you,
-      <a className="text-emerald-400 hover:text-emerald-300 underline ml-1" href="mailto:l.zeiger@crukscotlandinstitute.ac.uk">
+      <a className="text-emerald-400 hover:text-emerald-300 ml-1" href="mailto:l.zeiger@crukscotlandinstitute.ac.uk">
         send me an email
       </a>
       , or connect on
       <a
-        className="text-emerald-400 hover:text-emerald-300 underline ml-1"
+        className="text-emerald-400 hover:text-emerald-300 ml-1"
         href="https://linkedin.com/in/lucas-zeiger/"
         target="_blank"
         rel="noreferrer"
